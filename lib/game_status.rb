8745f144c1e08644f8e1df0d197d990f4572e8bb
Darius Stewart <darius.stewart31@gmail.com>
2019-08-27 06:12:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -74,16 +74,16 @@ def winner(board)
 
     if  position_1 == "X" && position_2 == "X" && position_3 == "X"
       return "X"
-    end
-    if position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return "O"
-    end
+    else
+      return nil 
   end
 
 
   board.all? do |token|
     if token.eql?(" ")
-      false 
+      false
     end
   end
 end
